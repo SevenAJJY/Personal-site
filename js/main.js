2bcead80f63899075d130519e206e89c624b7b88
SevenAJJY <yassine.elhajjy@gmail.com>
2022-07-28 03:13:00 +0100

    Added Skills & Services & projects Section

--- js/main.js
+++ js/main.js
@@ -31,3 +31,51 @@ const autoTyping = () => {
 
 }
 autoTyping();
+
+/************  Download CV *************/
+
+const downloadCV = () => {
+    let button = document.querySelector('.button');
+    button.addEventListener('click', (e) => {
+        button.classList.add('active-button');
+
+        setTimeout(() => {
+            let link = document.querySelector('.button-text');
+            button.classList.remove('active-button');
+            document.querySelector('.i-change').classList.replace('fa-cloud-arrow-down', 'fa-circle-check')
+            link.innerText = 'Completed';
+        }, 3000);
+    });
+}
+
+downloadCV();
+
+/************  Project Filter *************/
+const projectsFilter = () => {
+    const list = document.querySelectorAll(".list");
+    const itemBox = document.querySelectorAll(".itemBox");
+
+    for (let i = 0; i < list.length; i++) {
+        list[i].addEventListener("click", function() {
+            for (let j = 0; j < list.length; j++) {
+                list[j].classList.remove('projects-filter-active');
+            }
+            this.classList.add('projects-filter-active');
+
+            const dataFilter = this.getAttribute('data-filter');
+
+            for (let k = 0; k < itemBox.length; k++) {
+                itemBox[k].classList.remove('active');
+                itemBox[k].classList.add('hide');
+
+                if (itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                    console.log(dataFilter == "all");
+                    itemBox[k].classList.remove('hide');
+                    itemBox[k].classList.add('active');
+                }
+            }
+        });
+    }
+}
+
+projectsFilter();
