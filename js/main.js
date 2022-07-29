74200e3e7cd525c1fb0303294b486da0ca5ad9cf
SevenAJJY <yassine.elhajjy@gmail.com>
2022-07-29 17:00:33 +0100

    Added Responsive Section

--- js/main.js
+++ js/main.js
@@ -204,3 +204,28 @@ function isEmail(input) {
         return false;
     }
 }
+
+/************  Navigation *************/
+
+const navigation = () => {
+    const navToggler = document.querySelector('.js-nav-toggler');
+    const nav = document.querySelector('.js-nav');
+    const navItems = nav.querySelectorAll('li');
+
+    const navToggle = () => {
+        nav.classList.toggle('open');
+        navToggler.classList.toggle('active');
+    }
+
+    navToggler.addEventListener('click', navToggle);
+
+    navItems.forEach((li) => {
+        li.querySelector('a').addEventListener('click', () => {
+            if (window.innerWidth <= 767) {
+                navToggle();
+            }
+        });
+    });
+}
+
+navigation();
